6bd96eeef2e2bfe2d6cef006346f9df9634dac07
kekonen <daniil.naumetc@gmail.com>
2019-06-02 22:37:57 +0200

    testing inline sending, inline was sent

--- src/main.rs
+++ src/main.rs
@@ -24,19 +24,48 @@ fn main() {
     let future = api.stream().for_each(|update| {
         println!("3");
         // If the received update contains a new message...
-        if let UpdateKind::Message(message) = update.kind {
-            println!("4");
-            if let MessageKind::Text {ref data, ..} = message.kind {
-                println!("5");
-                // Print received text message to stdout.
-                println!("<{}>: {}", &message.from.first_name, data);
 
-                // Answer message with "Hi".
-                api.spawn(message.text_reply(
-                    format!("Hi, {}! You just wrote '{}'", &message.from.first_name, data)
-                ));
-            }
+        match update.kind {
+            UpdateKind::Message(message) => {
+                println!("Incoming message");
+                if let MessageKind::Text {ref data, ..} = message.kind {
+                    println!("5");
+                    // Print received text message to stdout.
+                    println!("<{}>: {}", &message.from.first_name, data);
+
+                    // Answer message with "Hi".
+                    api.spawn(message.text_reply(
+                        format!("Hi, {}! You just wrote '{}'", &message.from.first_name, data)
+                    ));
+                }
+            },
+            UpdateKind::InlineQuery(inline_query) => {
+                // InlineQuery { id: InlineQueryId("936883958196477061"),
+                // from: User { id: UserId(218135295), first_name: "Daniel", last_name: Some("Naumetc"), username: Some("kekonen"), is_bot: false, language_code: Some("en") },
+                // location: None, query: "", offset: "" }
+                // type TRequests = telegram_bot::types::requests
+
+                println!("id:{:?}", inline_query);
+                let kek = telegram_bot::types::InlineQueryResultVoice::new("kek", "Title", "https://file-examples.com/wp-content/uploads/2017/11/file_example_OOG_1MG.ogg");
+
+                let mut results = Vec::new();
+                results.push(
+                    telegram_bot::types::InlineQueryResult::InlineQueryResultVoice(
+                        kek
+                    )
+                );
+
+                api.spawn(telegram_bot::types::requests::AnswerInlineQuery::new(inline_query.id, results));
+                // api.spawn(inline_query);
+            },
+            _ => println!("kek"),
         }
+
+
+
+        // if let UpdateKind::InlineQuery(inline_query) = update.kind {
+        //     println!("4");
+        // }
 
         Ok(())
     });
